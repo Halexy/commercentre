79879ac2d88496409d97074343899c3d835b2801
Alexy Hajjar <alexyhajjar.ch@gmail.com>
2020-12-07 12:39:15 +0100

    Fix issue with file name not showing after image selection

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,10 @@ import './bootstrap';
 const $ = require('jquery');
 
 
+// Method for replacing the name of the input
+$('.custom-file-input').on('change', function(e) {
+    var inputFile = e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});
+
 
