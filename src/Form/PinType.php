05b55165d1bd7983d313d692a3e76538675504e0
Alexy Hajjar <alexyhajjar.ch@gmail.com>
2020-12-09 21:13:36 +0100

    explicit is better than implicit

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -7,18 +7,20 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', NULL, [
+            ->add('title', TextType::class, [
                 'label' => 'Titre'
             ])
             ->add('description')      
-            ->add('price', NULL, [
-                'label' => 'Prix (€)'
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix'
             ])     
             ->add('imageFile', VichImageType::class, [
             'label' => 'Image (Fichier JPG ou PNG)',
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,26 +4,28 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName', null, [
-                'label' => 'Prénom'
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
             ])
-            ->add('lastName', null, [
-                'label' => 'Nom'
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
             ])
-            ->add('email', null, [
+            ->add('email', EmailType::class, [
                 'help' => 'exemple@exemple.com',
             ])
             ->add('agreeTerms', CheckboxType::class, [
