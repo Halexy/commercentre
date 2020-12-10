5a172644443af93afb0a0b8dfdc5f83a5c7bb960
Alexy Hajjar <alexyhajjar.ch@gmail.com>
2020-12-10 01:01:58 +0100

    Style and translation reset password template

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -165,7 +165,7 @@ class ResetPasswordController extends AbstractController
                 $this->getParameter('app.mail_from_name')
             ))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
